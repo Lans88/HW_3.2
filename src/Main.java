a73380a9fe5fd9038bc4b07082a8356bb07e4e6b
Artem <art001.163@gmail.com>
2022-12-24 01:38:05 +0400

    rc-code BMI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BmiService index = new BmiService();
+        double m = 66.5;
+        double h = 1.76;
+        double ind = index.calculate(m, h);
+        System.out.println(ind);
     }
 }
